b0544703624859d63a1c2448ca504ffb9c154adb
Zoli06 <zolixvagyok@gmail.com>
2025-04-05 03:37:13 +0200

    Fix login layout

--- test_website/src/components/pages/UnauthenticatedPage.tsx
+++ test_website/src/components/pages/UnauthenticatedPage.tsx
@@ -2,8 +2,10 @@ import { Outlet } from "react-router";
 
 export function UnauthenticatedPage() {
   return (
-    <div className="flex flex-col items-stretch justify-center h-screen">
-      <Outlet />;
+    <div className="flex flex-col items-center justify-center h-screen w-screen">
+      <div className="w-xl">
+        <Outlet />
+      </div>
     </div>
   );
 }
